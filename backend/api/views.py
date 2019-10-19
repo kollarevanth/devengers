f0239a01fe35129ff6536e820e123ea85015a04b
revanthkolla123 <kollarevanth@gmail.com>
2019-10-19 23:41:16 +0530

    second_update

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+import json
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
 # Create your views here.
+@api_view(['POST'])
+def IdealWeight(heightdata):
+    try:
+        height=json.loads(heightdata.body)
+        weight=str(height*10)
+        return JsonResponse("Ideal weight should be:"+weight+" kg",safe=False)
+    except ValueError as e:
+        return Response(e.args[0],status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def Test(heightdata):
+    try:
+        return JsonResponse("Anil Sir",safe=False)
+    except ValueError as e:
+        return Response(e.args[0],status.HTTP_400_BAD_REQUEST)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+url(r'^idealweight/', views.IdealWeight),
+ url(r'^test/', views.Test),
 ]
+
+
+
